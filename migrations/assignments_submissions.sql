b06ea5cac398722c1142c0c2060515847fc4fd0c
Johannes Weber <johannes@drweber.de>
2022-01-13 19:13:38 -0800

    add seedfiles for assignments, assignment_submissions

--- migrations/assignments_submissions.sql
+++ migrations/assignments_submissions.sql
@@ -1,15 +1,5 @@
-DROP TABLE IF EXISTS assignments;
 
-CREATE TABLE assignments(
-  id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(255),
-  content TEXT,
-  day DATE,
-  chapter INTEGER,
-  duration INTEGER
-);
-
-DROP TABLE IF EXISTS assignment_submissions;
+/* DROP TABLE IF EXISTS assignment_submissions; */
 
 CREATE TABLE assignment_submissions(
   id SERIAL PRIMARY KEY NOT NULL,
@@ -17,4 +7,15 @@ CREATE TABLE assignment_submissions(
   student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
   duration INTEGER,
   submission_date DATE
+);
+
+/* DROP TABLE IF EXISTS assignments; */
+
+CREATE TABLE assignments(
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255),
+  content TEXT,
+  day INTEGER,
+  chapter INTEGER,
+  duration INTEGER
 );
