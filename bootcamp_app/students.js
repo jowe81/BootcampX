3224acd17d893354c7e778007aaade337cf76825
Johannes Weber <johannes@drweber.de>
2022-01-19 18:33:25 -0800

    parameterize queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -4,13 +4,16 @@ const pool = require('./database/connection.js');
 const cohort = process.argv[2];
 const limit = process.argv[3] || 5; //supply default to prevent syntax error on empty argument
 
-pool.query(`
-SELECT students.id, students.name, cohorts.name as cohort_name
-FROM students
-JOIN cohorts ON cohorts.id = students.cohort_id
-WHERE cohorts.name LIKE '%${cohort}%'
-LIMIT ${limit};
-`)
+const query = {
+  text: `SELECT students.id, students.name, cohorts.name as cohort_name
+  FROM students
+  JOIN cohorts ON cohorts.id = students.cohort_id
+  WHERE cohorts.name = $1
+  LIMIT $2`,
+  values: [cohort, limit]
+};
+
+pool.query(query)
   .then(res => {
     res.rows.forEach(student => {
       console.log(`${student.name} has an id of ${student.id} and they were in cohort ${student.cohort_name}.`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -2,14 +2,17 @@ const pool = require('./database/connection.js');
 
 const cohort = process.argv[2];
 
-pool.query(`
-SELECT DISTINCT teachers.name, cohorts.name AS cohort
+const query = {
+  text: `SELECT DISTINCT teachers.name, cohorts.name AS cohort
   FROM teachers
   JOIN assistance_requests ON assistance_requests.teacher_id = teachers.id
   JOIN students ON assistance_requests.student_id = students.id
   JOIN cohorts ON students.cohort_id = cohorts.id
-  WHERE cohorts.name = '${cohort}';
-`).then(res => {
+  WHERE cohorts.name = $1`,
+  values: [cohort]
+};
+
+pool.query(query).then(res => {
   res.rows.forEach(teacher => {
     console.log(`${cohort}: ${teacher.name}`);
   });
